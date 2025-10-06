fceb791ad8b13d6073f8d72b3376d652e5dd3134
Özay Melih Yıldız <102221334+Ozaui@users.noreply.github.com>
2025-10-06 22:47:45 +0300

    bugfixing

--- Day1.js
+++ Day1.js
@@ -30,13 +30,11 @@ for (let i = 0; i < numbers.length; i++) {}
 let min = numbers[0];
 let max = numbers[0];
 for (let i = 1; i < numbers.length; i++) {
-  if (numbers[i - 1] < numbers[i]) {
-    if (numbers[i] < min) {
-      min = numbers[i];
-    }
-    if (numbers[i] > max) {
-      max = numbers[i];
-    }
+  if (numbers[i] < min) {
+    min = numbers[i];
+  }
+  if (numbers[i] > max) {
+    max = numbers[i];
   }
 }
 
